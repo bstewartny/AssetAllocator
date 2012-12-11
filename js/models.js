4e825eb4a7c641f81c0a1ff20e53c8d9fcf5850a
Robert Stewart <bstewart.ny@gmail.com>
2012-12-11 14:22:22 -0500

    Created asset class profile

--- js/models.js
+++ js/models.js
@@ -1,24 +1,65 @@
 (function($){
+   
+    window.AssetAllocationProfile=Backbone.Model.extend({
+        //localStorage:new Store('profile'), 
+        defaults:{
+            assetClasses:[]             
+        },
+    initialize:function(){
+        },
+    addAssetClass:function(assetClass){
+        console.log('addAssetClass:'+assetClass.assetClass);
+        this.get('assetClasses').push(assetClass);
+        this.trigger('changeassetclasses',assetClass);
+        this.save();
+    }
+    ,
+    destroyAssetClass:function(obj){
+        var assetClasses=this.get('assetClasses');
+        assetClasses.splice(assetClasses.indexOf(obj),1);
+        this.trigger('changeassetclasses',obj);
+        this.save();
+    },
+    load:function(){
+         this.set('assetClasses',JSON.parse(
+             window.localStorage.getItem('assetClasses'))||[]);
+
+         },
+    save:function(){
+            console.log('save');
+         window.localStorage.setItem('assetClasses',JSON.stringify(this.get('assetClasses')));
+         
+         }
     
+    });
+
     window.Portfolio=Backbone.Model.extend({
         defaults:{
             assets:[]        
                  },
         initialize:function(){
                    
+        },
+        addAsset:function(asset){
+            this.get('assets').push(asset);
+            this.trigger('changeassets',asset);
+            this.save();
+        },
+        destroyAsset:function(asset){
+            var assets=this.get('assets');
+            assets.splice(assets.indexOf(asset),1);
+            this.trigger('changeassets',asset);
+            this.save();
         }
     });
 
     window.Portfolios=Backbone.Collection.extend({
         model:Portfolio,
-        //url:"portfolios",
-        localStorage:new Store('portfolios'), //new Store("portfolios"),
+        localStorage:new Store('portfolios'), 
         sumAssetClasses:function(){
             var assetClassMap={};
             // gather up asset classes from portfolios
-            for(var i=0;i<this.models.length;i++)
-            {
-                var portfolio=this.models[i];
+            _.each(this.models,function(portfolio){
                 var assets=portfolio.get('assets')
                 for(var j=0;j<assets.length;j++)
                 {
@@ -30,7 +71,7 @@
                     else
                         assetClassMap[assetClass]=value;
                 }
-            }
+            });
             // plot totals for each asset class into pie chart
             var data=[];
             for(var assetClass in assetClassMap)
@@ -57,20 +98,46 @@
                 return this;
             }
         });
+
+        window.AssetAllocationProfileView=Backbone.View.extend(
+        {
+            initialize:function()
+            {   
+                _.bindAll(this,'render');
+            },
+            appendAssetClass:function(asset){
+                console.log('appendAssetClass: '+asset.assetClass);
+                var that=this;
+                $('#asset-allocation-profile-assets').append($('<tr>')
+                    .append($('<td>',{'text':asset.assetClass}))
+                    .append($('<td>',{'text':asset.target}))
+                    .append($('<td>').append($('<i>',{'class':'icon-remove-sign'}).click(function(){if(confirm('Remove asset class '+asset.assetClass+'?')){ that.model.destroyAssetClass(asset);  }})).append(
+                            $('<i>',{'class':'icon-edit'}).click(function(){alert('edit asset class:'+asset.assetClass);})
+                    )));
+            },
+            render:function(){
+                  console.log('render');
+                  var that=this;
+                  $('#asset-allocation-profile-assets').html("");
+                  _.each(this.model.get('assetClasses'),function(asset){
+                    that.appendAssetClass(asset);
+                  });
+                  return this;
+            }
+        }
+        );
         window.PortfolioAssetView=Backbone.View.extend({
             initialize:function()
             {   
                 _.bindAll(this,'render');
-               // this.model.bind('destroy',this.remove);
             },
             appendAsset:function(asset){
                 var that=this;
-                var assets=that.model.get('assets');
                 $('#portfolio-assets').append($('<tr>')
                     .append($('<td>',{'text':asset.name}))
                     .append($('<td>',{'text':asset.assetClass}))
                     .append($('<td>',{'text':asset.currentValue}))
-                    .append($('<td>').append($('<i>',{'class':'icon-remove-sign'}).click(function(){if(confirm('Remove asset '+asset.name+'?')){ assets.splice(assets.indexOf(asset),1);that.render();plotAllocation(window.portfolios);  }})).append(
+                    .append($('<td>').append($('<i>',{'class':'icon-remove-sign'}).click(function(){if(confirm('Remove asset '+asset.name+'?')){ that.model.destroyAsset(asset);  }})).append(
                             $('<i>',{'class':'icon-edit'}).click(function(){alert('edit asset:'+asset.name);})
                     )));
             },
@@ -84,6 +151,7 @@
                   return this;
             }
         }),
+        
         window.PortfolioListView=Backbone.View.extend({
             el:'#portfolios',
             initialize:function(){
@@ -92,7 +160,7 @@
                 this.collection.bind('add',this.render);
             },
             appendPortfolio:function(portfolio){
-                 var v=new PortfolioView({model:portfolio});
+                var v=new PortfolioView({model:portfolio});
                  var e=v.render().el;
                  $(e).click(function(){
                      window.portfolio=portfolio;
@@ -112,10 +180,20 @@
         });
         
         window.portfolios=new Portfolios();
+        window.portfolios.bind('remove',function(event){
+            plotAllocation(window.portfolios);
+        });
         window.portfolioListView=new PortfolioListView({collection:window.portfolios});
-        window.portfolioAssetView=null; //new PortfolioAssetView();
+        window.portfolioAssetView=null; 
         window.portfolios.fetch();
+        window.portfolios.each(function(portfolio){
+            portfolio.on('changeassets',function(event){
+                window.portfolioAssetView.render();
+                plotAllocation(window.portfolios);
+            });
+        });
         window.portfolio=null;
+        window.profile=null;
         window.portfolioListView.render();
         if(window.portfolios.models.length>0)
         {
@@ -123,5 +201,16 @@
             window.portfolioAssetView=new PortfolioAssetView({model:window.portfolio});
             window.portfolioAssetView.render();
         }
+
+        
+        window.profile=new AssetAllocationProfile();
+        window.profile.load();
+
+        window.profileView=new AssetAllocationProfileView({model:window.profile});
+        window.profileView.render();
+
+        window.profile.on('changeassetclasses',function(event){
+            window.profileView.render();
+        });
     });
 })(jQuery);
