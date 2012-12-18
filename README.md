656c7918ad55f97bb4078ec74b107803e5689de1
Robert Stewart <bstewart.ny@gmail.com>
2012-12-18 14:46:27 -0500

    Refactor UI and show plot for each portfolio

---
+++ README.md
@@ -0,0 +1,13 @@
+# FinPlan
+
+A weekend/commuting project to help me allocate and rebalance my portfolio, and learn some new web technologies.
+
+### Architecture
+
+Client-side HTML5 single page app
+* Backbone.js, Underscore, Twitter Bootstrap 2.0, jQuery, Flot Chart library, HTML5 local storage, etc.
+
+### Install
+
+1. Clone this repo.
+2. Open index.htm in Chrome or some other HTML5 compliant browser. 
--- js/models.js
+++ js/models.js
@@ -1,14 +1,12 @@
 (function($){
    
     window.AssetAllocationProfile=Backbone.Model.extend({
-        //localStorage:new Store('profile'), 
         defaults:{
             assetClasses:[]             
         },
     initialize:function(){
         },
     addAssetClass:function(assetClass){
-        console.log('addAssetClass:'+assetClass.assetClass);
         this.get('assetClasses').push(assetClass);
         this.trigger('changeassetclasses',assetClass);
         this.save();
@@ -26,7 +24,6 @@
 
          },
     save:function(){
-            console.log('save');
          window.localStorage.setItem('assetClasses',JSON.stringify(this.get('assetClasses')));
          
          }
@@ -40,7 +37,31 @@
         initialize:function(){
                    
         },
+        sumAssetClasses:function(){
+            var assetClassMap={};
+            // gather up asset classes from portfolios
+            var assets=this.get('assets')
+            for(var j=0;j<assets.length;j++)
+            {
+                var asset=assets[j];
+                var assetClass=asset.assetClass;
+                var value=Number(asset.currentValue);
+                if (assetClass in assetClassMap)
+                    assetClassMap[assetClass]+=value;
+                else
+                    assetClassMap[assetClass]=value;
+            }
+            // plot totals for each asset class into pie chart
+            var data=[];
+            for(var assetClass in assetClassMap)
+            {
+                data[data.length]={ label:assetClass, data:assetClassMap[assetClass]};
+            }
+            return data;
+        },
+        
         addAsset:function(asset){
+            console.log('portfolio.addAsset');
             this.get('assets').push(asset);
             this.trigger('changeassets',asset);
             this.save();
@@ -56,6 +77,26 @@
     window.Portfolios=Backbone.Collection.extend({
         model:Portfolio,
         localStorage:new Store('portfolios'), 
+        getAssetClassValue:function(assetClass){
+            // get total value for this asset class from all portfolios
+            var assetTotal=0;
+            var total=0;
+            // also get total value of all portfolios to calculate asset class allocation
+            _.each(this.models,function(portfolio){
+                var assets=portfolio.get('assets')
+                for(var j=0;j<assets.length;j++)
+                {
+                    var asset=assets[j];
+                    if(asset.assetClass==assetClass)
+                    {
+                        assetTotal+=asset.currentValue;
+                    }
+                    total+=asset.currentValue;
+                } 
+            });
+            return {'currentValue':assetTotal,'percentage':(assetTotal/total)};
+         }   
+        ,
         sumAssetClasses:function(){
             var assetClassMap={};
             // gather up asset classes from portfolios
@@ -87,70 +128,113 @@
        
         window.PortfolioView=Backbone.View.extend({
             template:_.template($('#portfolio-template').html()),
-            tagName:'li',
-            className:'portfolio',
+            tagName:'div',
+            className:'row-fluid',
+
+            events:{
+                "click .add-asset-btn":"addAsset", 
+                "keypress .currentValue":"updateOnEnter",
+                "blur .currentValue":"close"
+            
+            
+            },
+
+            addAsset:function(){
+                try{
+                var portfolio=this.model;
+                if(!portfolio) return false;
+                var name=this.$('.add-asset-name').val();
+                var assetClass=this.$('.add-asset-class').val();
+                var currentValue=Number(this.$('.add-asset-value').val());
+                if(!name || !assetClass || !currentValue)
+                {
+                    alert("Please enter all form fields.");
+                    return false;
+                }
+                portfolio.addAsset({'name':name,'assetClass':assetClass,'currentValue':currentValue});
+                this.$('.add-asset-name').val('');
+                this.$('.add-asset-class').val('');
+                this.$('.add-asset-value').val('');
+                this.render();
+                }
+                catch(e)
+                {
+                    console.log(e);
+                }
+                return false; 
+            },
+            updateOnEnter:function(e){
+              if(e.keyCode==13) {
+                  this.close();
+              }
+            },
+            close:function(e){
+                $.each(this.$('.currentValue'),function(i,input){
+                    var asset=$(input).data('asset');
+                    if(asset)
+                    {
+                        asset.currentValue=Number($(input).val());
+                    }
+                });
+                this.model.trigger('changeassets',this);
+                this.model.save();
+                this.renderChart();
+            },
             initialize:function(){
                 _.bindAll(this,'render');
                 this.model.bind('change',this.render);
             },
+            renderChart:function(){
+                            try{
+                $.plot(this.$('.portfolio-allocation-pie'),this.model.sumAssetClasses(),{ series:{pie:{show:true}}   });
+                            }
+                            catch(e)
+                            {
+                                console.log('renderChart failed');
+                            }
+            },
             render:function(){
                 $(this.el).html(this.template(this.model.toJSON()));
+                var assets=this.model.get('assets');
+                $.each(this.$('.currentValue'),function(i,input){
+                    $(input).data('asset',assets[i]);
+                });
+                this.$('.portfolio-allocation-pie').data('portfolio',this.model);
+                //$.plot(this.$('.portfolio-allocation-pie'),this.model.sumAssetClasses(),{ series:{pie:{show:true}}   });
+                this.renderChart();
                 return this;
             }
         });
 
         window.AssetAllocationProfileView=Backbone.View.extend(
         {
+            el:'#asset-allocation-profile',
+            template:_.template($('#asset-allocation-profile-template').html()),
+
             initialize:function()
             {   
                 _.bindAll(this,'render');
             },
             appendAssetClass:function(asset){
-                console.log('appendAssetClass: '+asset.assetClass);
                 var that=this;
+                var assetValue=window.portfolios.getAssetClassValue(asset.assetClass);
+
                 $('#asset-allocation-profile-assets').append($('<tr>')
                     .append($('<td>',{'text':asset.assetClass}))
                     .append($('<td>',{'text':asset.target}))
+                    .append($('<td>',{'text':assetValue.currentValue}))
+                    .append($('<td>',{'text':Math.floor(assetValue.percentage*100.0)}))
                     .append($('<td>').append($('<i>',{'class':'icon-remove-sign'}).click(function(){if(confirm('Remove asset class '+asset.assetClass+'?')){ that.model.destroyAssetClass(asset);  }})).append(
                             $('<i>',{'class':'icon-edit'}).click(function(){alert('edit asset class:'+asset.assetClass);})
                     )));
             },
             render:function(){
-                  console.log('render');
-                  var that=this;
-                  $('#asset-allocation-profile-assets').html("");
-                  _.each(this.model.get('assetClasses'),function(asset){
-                    that.appendAssetClass(asset);
-                  });
+                       console.log('render profile');
+                $(this.el).html(this.template(this.model.toJSON()));
                   return this;
             }
         }
         );
-        window.PortfolioAssetView=Backbone.View.extend({
-            initialize:function()
-            {   
-                _.bindAll(this,'render');
-            },
-            appendAsset:function(asset){
-                var that=this;
-                $('#portfolio-assets').append($('<tr>')
-                    .append($('<td>',{'text':asset.name}))
-                    .append($('<td>',{'text':asset.assetClass}))
-                    .append($('<td>',{'text':asset.currentValue}))
-                    .append($('<td>').append($('<i>',{'class':'icon-remove-sign'}).click(function(){if(confirm('Remove asset '+asset.name+'?')){ that.model.destroyAsset(asset);  }})).append(
-                            $('<i>',{'class':'icon-edit'}).click(function(){alert('edit asset:'+asset.name);})
-                    )));
-            },
-            render:function(){
-                  var that=this;
-                  $('#portfolio-detail-name').html(this.model.get('name')).append($('<i>',{'class':'icon-remove-sign'}).click(function(){if(confirm('Delete portfolio?')){that.model.destroy();}}));
-                  $('#portfolio-assets').html("");
-                  _.each(this.model.get('assets'),function(asset){
-                    that.appendAsset(asset);
-                  });
-                  return this;
-            }
-        }),
         
         window.PortfolioListView=Backbone.View.extend({
             el:'#portfolios',
@@ -162,12 +246,8 @@
             appendPortfolio:function(portfolio){
                 var v=new PortfolioView({model:portfolio});
                  var e=v.render().el;
-                 $(e).click(function(){
-                     window.portfolio=portfolio;
-                     window.portfolioAssetView=new PortfolioAssetView({model:window.portfolio});
-                     window.portfolioAssetView.render();
-                 });
-                $(this.el).append(e); 
+                $(this.el).append(e);
+                v.renderChart();
             },
             render:function(){
                   $(this.el).html('');
@@ -184,23 +264,18 @@
             plotAllocation(window.portfolios);
         });
         window.portfolioListView=new PortfolioListView({collection:window.portfolios});
-        window.portfolioAssetView=null; 
         window.portfolios.fetch();
         window.portfolios.each(function(portfolio){
             portfolio.on('changeassets',function(event){
-                window.portfolioAssetView.render();
+                console.log('changeassets fired');
+                window.profileView.render();
+                
                 plotAllocation(window.portfolios);
             });
         });
         window.portfolio=null;
         window.profile=null;
         window.portfolioListView.render();
-        if(window.portfolios.models.length>0)
-        {
-            window.portfolio=window.portfolios.models[0];
-            window.portfolioAssetView=new PortfolioAssetView({model:window.portfolio});
-            window.portfolioAssetView.render();
-        }
 
         
         window.profile=new AssetAllocationProfile();
